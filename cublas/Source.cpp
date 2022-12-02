a6b24fa1994c9aa60495a039e3a96afa02976cc8
theProgrammingBox <justinboxgea@gmail.com>
2022-12-01 23:44:33 -0800

    added matmulmat, matTmulmat, matmulmatT

--- cublas/Source.cpp
+++ cublas/Source.cpp
@@ -10,106 +10,166 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::nanoseconds;
 
-/*
-write a program that uses cublas to multiply two matrices
-A x B = C
-a x b * b x c = a x c
-*/
-
 int main() {
-	// initialize random number generator
 	curandGenerator_t gen;
 	curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
 	curandSetPseudoRandomGeneratorSeed(gen, duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count());
-
-	// initialize cublas
+	
 	cublasHandle_t handle;
 	cublasCreate(&handle);
-
-	// initialize matrices
-	int a = 1 << 12;
-	int b = 1 << 12;
-	int c = 1 << 12;
+	
+	float elapsedTime;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	
+	float alpha = 1.0f;
+	float beta = 0.0f;
+	int maxIterations = 100;
+	
+	int a = 500;
+	int b = 700;
+	int c = 300;
+	
 	float* A, * B, * C;
 	cudaMallocManaged(&A, a * b * sizeof(float));
 	cudaMallocManaged(&B, b * c * sizeof(float));
 	cudaMallocManaged(&C, a * c * sizeof(float));
+	
+	float* hC = new float[a * c];
+	float* hA = new float[a * b];
+	float* hB = new float[b * c];
+	
+	
 
-	// fill matrices with random numbers
+	// matrix times matrix
 	curandGenerateUniform(gen, A, a * b);
 	curandGenerateUniform(gen, B, b * c);
 
-	// multiply matrices
-	float elapsedTime;
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
+	int iterations = maxIterations;
 	cudaEventRecord(start, 0);
-	
-	float alpha = 1.0f;
-	float beta = 0.0f;
-	int iterations = 10;
 	while (iterations--) {
 		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, c, a, b, &alpha, B, c, A, b, &beta, C, c);
 	}
-	
-	cudaEventCreate(&stop);
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&elapsedTime, start, stop);
-	cout << "Time: " << elapsedTime / 10 << " ms" << endl;
-
-	// copy to host
-	float* hC = new float[a * c];
-	cudaMemcpy(hC, C, a * c * sizeof(float), cudaMemcpyDeviceToHost);
-	float* hA = new float[a * b];
+	cout << "Time: " << elapsedTime / maxIterations << " ms" << endl;
+	
 	cudaMemcpy(hA, A, a * b * sizeof(float), cudaMemcpyDeviceToHost);
-	float* hB = new float[b * c];
 	cudaMemcpy(hB, B, b * c * sizeof(float), cudaMemcpyDeviceToHost);
-
-	// calculate result on host
-	float* hC2 = new float[a * c];
+	cudaMemcpy(hC, C, a * c * sizeof(float), cudaMemcpyDeviceToHost);
+	
+	float* output = new float[a * c];
 	for (int i = 0; i < a; i++) {
 		for (int j = 0; j < c; j++) {
 			float sum = 0;
 			for (int k = 0; k < b; k++) {
 				sum += hA[i * b + k] * hB[k * c + j];
 			}
-			hC2[i * c + j] = sum;
+			output[i * c + j] = sum;
 		}
 	}
 	
-	// print average error
 	float error = 0;
 	for (int i = 0; i < a * c; i++) {
-		error += abs(hC[i] - hC2[i]);
+		error += abs(hC[i] - output[i]);
 	}
 	cout << "Average error: " << error / (a * c) << endl;
+	
+	delete[] output;
 
-	/*cout << "A:" << endl;
-	for (int i = 0; i < a; i++) {
-		for (int j = 0; j < b; j++) {
-			cout << hA[i * b + j] << " ";
-		}
-		cout << endl;
+
+	
+	// transposed matrix times matrix
+	curandGenerateUniform(gen, C, a * c);
+	curandGenerateUniform(gen, A, a * b);
+
+	iterations = maxIterations;
+	cudaEventRecord(start, 0);
+	while (iterations--) {
+		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, c, b, a, &alpha, C, c, A, b, &beta, B, c);
 	}
-	cout << "B:" << endl;
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+	cout << "Time: " << elapsedTime / maxIterations << " ms" << endl;
+	
+	cudaMemcpy(hA, A, a * b * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hB, B, b * c * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hC, C, a * c * sizeof(float), cudaMemcpyDeviceToHost);
+	
+	output = new float[b * c];
 	for (int i = 0; i < b; i++) {
 		for (int j = 0; j < c; j++) {
-			cout << hB[i * c + j] << " ";
+			float sum = 0;
+			for (int k = 0; k < a; k++) {
+				sum += hA[k * b + i] * hC[k * c + j];
+			}
+			output[i * c + j] = sum;
 		}
-		cout << endl;
-	}*/
+	}
+	
+	error = 0;
+	for (int i = 0; i < b * c; i++) {
+		error += abs(hB[i] - output[i]);
+	}
+	cout << "Average error: " << error / (b * c) << endl;
 
-	// free memory
+	delete[] output;
+
+	
+	
+	// matrix times transposed matrix
+	curandGenerateUniform(gen, C, a * c);
+	curandGenerateUniform(gen, B, b * c);
+	
+	iterations = maxIterations;
+	cudaEventRecord(start, 0);
+	while (iterations--) {
+		cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, b, a, c, &alpha, B, c, C, c, &beta, A, b);
+	}
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsedTime, start, stop);
+	cout << "Time: " << elapsedTime / maxIterations << " ms" << endl;
+	
+	cudaMemcpy(hA, A, a * b * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hB, B, b * c * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hC, C, a * c * sizeof(float), cudaMemcpyDeviceToHost);
+	
+	output = new float[a * b];
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			float sum = 0;
+			for (int k = 0; k < c; k++) {
+				sum += hC[i * c + k] * hB[j * c + k];
+			}
+			output[i * b + j] = sum;
+		}
+	}
+	
+	error = 0;
+	for (int i = 0; i < a * b; i++) {
+		error += abs(hA[i] - output[i]);
+	}
+	cout << "Average error: " << error / (a * b) << endl;
+
+	delete[] output;
+	
+
+	
 	cudaFree(A);
 	cudaFree(B);
 	cudaFree(C);
-	delete[] hC;
 	delete[] hA;
 	delete[] hB;
-	delete[] hC2;
-	cublasDestroy(handle);
+	delete[] hC;
+	
 	curandDestroyGenerator(gen);
+	cublasDestroy(handle);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 
 	return 0;
 }
